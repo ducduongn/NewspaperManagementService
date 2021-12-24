1685038d6a14e7f8546d31ce208283b546ed3363
ducduongn <48018619+ducduongn@users.noreply.github.com>
2021-12-24 14:55:01 +0700

    Update URLConstant.java

--- src/main/java/com/example/springsecuritydemo/constant/cralwer/URLConstant.java
+++ src/main/java/com/example/springsecuritydemo/constant/cralwer/URLConstant.java
@@ -4,7 +4,7 @@ package com.example.springsecuritydemo.constant.cralwer;
  * @author ducduongn
  */
 public class URLConstant {
-    public static final String htmlPath = "D:\\Learning\\ElcomIntern\\SpringLearningServices\\SpringSecurityDemo\\src\\main\\java\\com\\example\\springsecuritydemo\\constant\\cralwer\\Menu.html";
+    public static final String htmlPath = "src/main/java/com/example/springsecuritydemo/constant/cralwer/Menu.html";
 
     public static final String VN_EXPRESS_HOME = "https://vnexpress.net/";
 
