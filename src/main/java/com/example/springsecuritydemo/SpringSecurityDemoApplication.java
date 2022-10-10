95c440f991f85e88fba50d672c7d7b616ff8d5e5
ducduongn <48018619+ducduongn@users.noreply.github.com>
2022-10-10 22:19:05 +0700

    git update docker compose

--- src/main/java/com/example/springsecuritydemo/SpringSecurityDemoApplication.java
+++ src/main/java/com/example/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -23,24 +23,24 @@ public class SpringSecurityDemoApplication {
         SpringApplication.run(SpringSecurityDemoApplication.class, args);
     }
 
-    @PostConstruct
-    public void initializeRoles() {
-        List<Role> roleList = new ArrayList<>();
-
-        Role adminRole = new Role(ERole.ROLE_ADMIN);
-        Role journalistRole = new Role(ERole.ROLE_JOURNALIST);
-        Role editorRole = new Role(ERole.ROLE_EDITOR);
-        Role directorRole = new Role(ERole.ROLE_DIRECTOR);
-
-        roleList.add(adminRole);
-        roleList.add(journalistRole);
-        roleList.add(editorRole);
-        roleList.add(directorRole);
-
-        for (Role role:roleList) {
-            if (!roleRepository.existsByRoleName(role.getRoleName())) {
-                roleRepository.save(role);
-            }
-        }
-    }
+//    @PostConstruct
+//    public void initializeRoles() {
+//        List<Role> roleList = new ArrayList<>();
+//
+//        Role adminRole = new Role(ERole.ROLE_ADMIN);
+//        Role journalistRole = new Role(ERole.ROLE_JOURNALIST);
+//        Role editorRole = new Role(ERole.ROLE_EDITOR);
+//        Role directorRole = new Role(ERole.ROLE_DIRECTOR);
+//
+//        roleList.add(adminRole);
+//        roleList.add(journalistRole);
+//        roleList.add(editorRole);
+//        roleList.add(directorRole);
+//
+//        for (Role role:roleList) {
+//            if (!roleRepository.existsByRoleName(role.getRoleName())) {
+//                roleRepository.save(role);
+//            }
+//        }
+//    }
 }
